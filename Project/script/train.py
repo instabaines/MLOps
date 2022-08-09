92f4a4a92c80df0b3c3cb52985f87701c4c0e53b
instabaines <Adlordromeo@gmail.com>
2022-08-09 23:47:25 +0100

    updated script to track experiment

--- Project/script/train.py
+++ Project/script/train.py
@@ -1,4 +1,8 @@
 from prepare import prepare_data
+import mlflow
+
+mlflow.set_tracking_uri("sqlite:///backend.db")
+mlflow.set_experiment("random_forest_experiment")
 #import the necessary libraries
 
 import numpy as np
@@ -14,13 +18,8 @@ def rmspe_xg(yhat, y):
     yhat = np.expm1(yhat)
     return "rmspe", rmspe(y,yhat)
 
-def train_model(x_train,y_train,x_valid,y_valid):
-    clf = RandomForestRegressor(n_estimators = 15)
-    clf.fit(x_train, y_train)
-    # validation
-    y_pred = clf.predict(x_valid)
-    error = rmspe(np.expm1(y_valid), np.expm1(y_pred))
-    print('RMSPE: {:.4f}'.format(error))
+# def train_model(x_train,y_train,x_valid,y_valid):
+
 
 def prepare_data_for_training(train):
     split_index = 6*7*1115
@@ -34,13 +33,25 @@ def prepare_data_for_training(train):
     return x_train,y_train,x_valid,y_valid
    
 def train():
-    train=prepare_data('Project/data/raw/train.csv','Project/data/raw/store.csv')
-    x_train,y_train,x_valid,y_valid = prepare_data_for_training(train)
-    train_model(x_train,y_train,x_valid,y_valid)
+    with mlflow.start_run():
+        paths=['Project/data/raw/train.csv','Project/data/raw/store.csv']
+        train=prepare_data(paths[0],paths[1])
+        mlflow.log_param('path_to_data',paths)
+        n_estimators = 15
+        mlflow.log_param('n_estimators',n_estimators)
+        x_train,y_train,x_valid,y_valid = prepare_data_for_training(train)
+        clf = RandomForestRegressor(n_estimators = n_estimators )
+        clf.fit(x_train, y_train)
+        # validation
+        y_pred = clf.predict(x_valid)
+        error = rmspe(np.expm1(y_valid), np.expm1(y_pred))
+        mlflow.log_metric("error", error)
+        print('RMSPE: {:.4f}'.format(error))
+            
 
+        mlflow.sklearn.log_model(clf, artifact_path="models")
+    print(f"default artifacts URI: '{mlflow.get_artifact_uri()}'")
 
-    # split x and y
-    x_train, y_train = train.drop(columns = ['Sales']), np.log1p(train['Sales'])
 
 if __name__=='__main__':
     train()
